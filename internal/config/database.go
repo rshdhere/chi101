d149eefdf619f85a64481908e299912ed022ae2c
raashed <mohammedraashed10@gmail.com>
2025-12-10 10:32:08 +0530

    feat(config): add DBSecret struct for database credentials
    - Introduce DBSecret struct with fields for username, password, database, host, and port
    - Provide JSON tags for easy serialization and deserialization of database secrets
    - Centralize database connection configuration in a dedicated struct within the config package

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,2 +1,10 @@
 // Package config deals with all the database logic
 package config
+
+type DBSecret struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Database string `json:"database"`
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+}
