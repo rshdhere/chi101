e56ade6dbf389b5431dc88cdf7bc09a3201367d2
raashed <mohammedraashed10@gmail.com>
2025-12-10 09:53:18 +0530

    feat(main): initialize application instance in main function - Import internal app package and create a new application instance - Add error handling for application initialization with panic on failure - Retain existing port flag parsing for server configuration - Prepare main.go for further application setup and server start logic integration

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/rshdhere/chi101/internal/app"
+)
 
 func main() {
 	var PORT int
 
 	flag.IntVar(&PORT, "port", 8080, "port for a go-backend")
 	flag.Parse()
+
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
 }
