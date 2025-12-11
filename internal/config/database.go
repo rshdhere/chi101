a953a6976461a2b01f38fc44809b2d17e4a9434d
raashed <mohammedraashed10@gmail.com>
2025-12-11 23:22:32 +0530

    refactor(config): remove unused DBSecret struct and related constants from database.go - Delete DBSecret struct which contained database credentials fields - Remove environment variable and secret name constants related to database configuration - Simplify internal/config/database.go by removing unused code related to secret management - Prepare for alternative or external configuration management approaches for database credentials handling

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,22 +1,2 @@
 // Package config deals with all the database logic
 package config
-
-type DBSecret struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Database string `json:"database"`
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-}
-
-const (
-	secretNameProd    = "prod/gym/postgres"
-	secretNameStaging = "stagging/gym/postgres"
-
-	secretNameEnvVar = "DB_SECRET_NAME"
-
-	appEnvVar     = "APP_ENV"
-	localEnvVal   = "local"
-	stagingEnvVal = "staging"
-	prodEnvVal    = "prod"
-)
